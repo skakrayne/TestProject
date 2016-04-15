779ff73545382e0b8bb7498b2fde7caf7a05af8f
Saikatdey12 <amitkumar@Sakis-MacBook-Pro.local>
2016-04-15 15:01:17 +0530

    Committing Login scale UI

--- index.ios.js
+++ index.ios.js
@@ -3,50 +3,94 @@
  * https://github.com/facebook/react-native
  */
 
+'use strict';
+
+
+// var React = require('react-native');
+
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  ScrollView,
+  TextInput,
 } from 'react-native';
 
-class TestProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+var styles = StyleSheet.create({
+    text:{
+        fontSize:18 
+    },
+    container:{
+        flex:1,
+        margin: 30,
+    },
+    heading:{
+        fontSize:20 ,
+        marginBottom: 30,
+        color:'black',
+        marginTop: 70
+    },
+    bottom_margin:{
+        marginBottom: 20
+    },
+    buttonText: {
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+    },
+    button: {
+        height: 36,
+        marginTop: 40,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+    },
+     textField1: {
+        height: 40, 
+        borderColor: 'gray', 
+        borderWidth: 1,
+        marginBottom:20,
+        marginTop:20       
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
-AppRegistry.registerComponent('TestProject', () => TestProject);
+
+
+class LoginPage extends React.Component{
+    render(){
+        var layout =
+            <ScrollView style = { styles.container } >
+    
+                <Text style={styles.heading}>
+                    Please Enter your Login Credentials
+                </Text>
+                
+                <Text style={styles.text}>
+                    Username
+                </Text>
+                <React.TextInput style={styles.textField1}/>
+    
+                <Text style={styles.text}>
+                    Password
+                </Text>
+                
+                <React.TextInput style={styles.textField1}/>
+                
+                <TouchableHighlight style={styles.button}
+                    underlayColor='#99d9f4'>
+                <Text style={styles.buttonText}>Login</Text>
+                </TouchableHighlight>
+             </ScrollView>
+        ;
+    return layout;
+    }
+};
+
+AppRegistry.registerComponent('TestProject' , function(){return LoginPage});
